d7b92d315819bc30912cf4eab6b72e238bbc65ce
thedtv-1961 <51468300+thedtv-1961@users.noreply.github.com>
2021-12-16 06:06:28 +0700

    Update c4f_Async_Future_FutureBuilder.dart

--- flutter/c4f_Async_Future_FutureBuilder.dart
+++ flutter/c4f_Async_Future_FutureBuilder.dart
@@ -1,3 +1,90 @@
+// Mạc định chương trình sẽ chạy mà ko chờ
+
+void main() {
+  print('bat dau goi mon');
+  print(goiDoAn());
+  print('Cho goi mon...');
+}
+
+String goiDoAn() {
+  print('goiDoAn');
+  var monAn = lamDoAn();
+
+  return 'Mon an $monAn da dc goi';
+}
+
+Future<String> lamDoAn() {
+  return Future.delayed(Duration(seconds: 7), () => 'Banh Mi 7 phut');
+}
+
+// bat dau goi mon
+// goiDoAn
+// Mon an Instance of 'Future<String>' da dc goi
+// Cho goi mon...
+// <<<< pause 7 minutes >>>>
+// Exited
+
+
+// Dùng async/await chương trình sẽ chờ hàm `async`
+
+void main() async {
+  print('bat dau goi mon');
+  print(await goiDoAn());
+  print('Cho goi mon...');
+}
+
+Future<String> goiDoAn() async {
+  print('goiDoAn');
+  var monAn = await lamDoAn();
+
+  return 'Mon an $monAn da dc goi';
+}
+
+Future<String> lamDoAn() {
+  return Future.delayed(Duration(seconds: 7), () => 'Banh Mi 7 phut');
+}
+
+// bat dau goi mon
+// goiDoAn
+// <<<< pause 7 minutes >>>>
+// Mon an Banh Mi 7 phut da dc goi
+// Cho goi mon...
+// Exited
+
+// Dùng .then() chương trình sẽ chạy 1 lượt, các hàm là `async` sẽ chạy background và show ra sau.
+
+void main() {
+  print('bat dau goi mon');
+  goiDoAn().then((val) {
+    print(val);
+  });
+  print('Cho goi mon...');
+}
+
+Future<String> goiDoAn() async {
+  print('goiDoAn');
+  var monAn = await lamDoAn();
+
+  return 'Mon an $monAn da dc goi';
+}
+
+Future<String> lamDoAn() {
+  return Future.delayed(Duration(seconds: 7), () => 'Banh Mi 7 phut');
+}
+
+// bat dau goi mon
+// goiDoAn
+// Cho goi mon...
+// <<<< pause 7 minutes >>>>
+// Mon an Banh Mi 7 phut da dc goi
+// Exited
+
+
+
+
+
+
+
 // Future là giá trị đc trả về trong tương lai.
 /////////////////////////Future/////////////////////////////
 void main(List<String> arguments) {
